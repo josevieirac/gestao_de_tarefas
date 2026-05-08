bf6a94532f2e4257e0235be7f55bd26da4dfde8e
José Vieira <josevieira1709@gmail.com>
2026-05-08 19:08:02 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Projeto Git JS
+
+Projeto simples para praticar Git e JavaScript.
+
+## Estrutura do código
+projeto-git-js/
+├── index.html
+├── script.js
+└── README.md
+
+## Funcionalidades
+- Adicionar tarefas
+- Listar tarefas
+
+## Objetivo
+Aprender Git na prática com evolução do código.
---
+++ script.js
@@ -0,0 +1,27 @@
+let tasks = [];
+
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value;
+
+  if (taskText === "") {
+    alert("Digite uma tarefa válida!");
+    return;
+  }
+
+  tasks.push(taskText);
+  input.value = "";
+  renderTasks();
+}
+
+function renderTasks() {
+  const list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    list.appendChild(li);
+  }); 
+}
